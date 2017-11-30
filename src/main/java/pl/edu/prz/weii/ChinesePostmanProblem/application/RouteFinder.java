233e266d096be813e965cd489d3536182f9a7365
sylwekczmil <sylwekczmil@gmail.com>
2017-11-30 19:39:02 +0100

    Refactor

--- src/main/java/pl/edu/prz/weii/ChinesePostmanProblem/application/RouteFinder.java
+++ src/main/java/pl/edu/prz/weii/ChinesePostmanProblem/application/RouteFinder.java
@@ -11,13 +11,16 @@ import pl.edu.prz.weii.ChinesePostmanProblem.domain.graph.Route;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.jenetics.engine.limit.bySteadyFitness;
+
 public class RouteFinder {
 
     private Set<Edge> edges = new HashSet<>();
     private Set<Integer> nodes = new HashSet<>();
 
     private int populationSize = 150;
-    private long limit = 20_000;
+    private long limitIterations = 100_000_000;
+    private int limitSteady = 10_000;
     private double probabilityOfMutation = 0.3;
 
     public RouteFinder(FileContent fileContent) {
@@ -25,10 +28,11 @@ public class RouteFinder {
         this.nodes = fileContent.getNodes();
     }
 
-    public RouteFinder(FileContent fileContent, int populationSize, long limit, double probabilityOfMutation) {
+    public RouteFinder(FileContent fileContent, int populationSize, long limitIterations, int limitSteady,  double probabilityOfMutation) {
         this(fileContent);
         this.populationSize = populationSize;
-        this.limit = limit;
+        this.limitIterations = limitIterations;
+        this.limitSteady = limitSteady;
         this.probabilityOfMutation = probabilityOfMutation;
     }
 
@@ -49,9 +53,9 @@ public class RouteFinder {
                 .build();
 
         final EvolutionResult<IntegerGene, Double> result = engine.stream()
-                .limit(this.limit)
+                .limit(bySteadyFitness(this.limitSteady))
+                .limit(this.limitIterations)
                 .collect(EvolutionResult.toBestEvolutionResult());
-
         Genotype<IntegerGene> genotype = result.getBestPhenotype().getGenotype();
         return Route.code(this.nodes, this.edges).decoder().apply(genotype);
     }
